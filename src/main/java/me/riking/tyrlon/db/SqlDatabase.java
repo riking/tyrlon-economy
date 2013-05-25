29c95a0cf053dad0669f1a20d5f288f54767e90e
riking <rikingcoding@gmail.com>
2013-05-25 11:46:26 -0700

    Add prepareStatements() to abstract SqlDatabase

--- src/main/java/me/riking/tyrlon/db/SqlDatabase.java
+++ src/main/java/me/riking/tyrlon/db/SqlDatabase.java
@@ -3,12 +3,23 @@ package me.riking.tyrlon.db;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 import me.riking.tyrlon.Database;
 
 public abstract class SqlDatabase implements Database {
     Connection connection;
-    PreparedStatement savePlayer;
     String database;
+
+    PreparedStatement newPlayer;
+    PreparedStatement savePlayer;
+    PreparedStatement deletePlayer;
+    PreparedStatement newBank;
+    PreparedStatement saveBankMoney;
+    PreparedStatement saveBankFull;
+    PreparedStatement deleteBank;
+    PreparedStatement getPlayers;
+    PreparedStatement getBanks;
 
     public SqlDatabase(Connection connection, String database) {
         this.connection = connection;
@@ -25,14 +36,21 @@ public abstract class SqlDatabase implements Database {
         useDb.execute();
         createDb.close();
         useDb.close();
-        connection.createStatement().execute("CREATE TABLE players IF NOT EXISTS (" +
-        		"name varchar(200) NOT NULL," +
-        		"money double precision NOT NULL" +
-        		")");
-        connection.createStatement().execute("CREATE TABLE banks IF NOT EXISTS (" +
-        		"bankname varchar(200) NOT NULL," +
-        		"owner varchar(200)," +
-        		"FOREIGN KEY (owner) REFERENCES players(name)" +
-        		")");
+        Statement s1 = connection.createStatement();
+        s1.execute("CREATE TABLE players IF NOT EXISTS ( name varchar(200) NOT NULL PRIMARY KEY, money double precision NOT NULL )");
+        s1.execute("CREATE TABLE banks IF NOT EXISTS ( bankname varchar(200) NOT NULL PRIMARY KEY, owner varchar(200), money double precision NOT NULL, FOREIGN KEY (owner) REFERENCES players(name) )");
+        s1.close();
+    }
+
+    public void prepareStatements() throws SQLException {
+        newPlayer = connection.prepareStatement("INSERT INTO players(name, money) VALUES (?, ?)");
+        newBank = connection.prepareStatement("INSERT INTO BANKS(bankname, owner, money) VALUES (?, ?, ?)");
+        savePlayer = connection.prepareStatement("UPDATE players SET money=? WHERE name=?");
+        saveBankMoney = connection.prepareStatement("UPDATE banks SET money=? WHERE bankname=?");
+        saveBankFull = connection.prepareStatement("UPDATE banks SET money=?,owner=? WHERE bankname=?");
+        deletePlayer = connection.prepareStatement("DELETE FROM players WHERE name=?");
+        deletePlayer = connection.prepareStatement("DELETE FROM banks WHERE name=?");
+        getPlayers = connection.prepareStatement("SELECT name, money FROM players");
+        getBanks = connection.prepareStatement("SELECT bankname, owner, money FROM banks");
     }
 }
